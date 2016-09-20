ef6d76f41e3860b84a190c11ff4a155a7ccda630
Rui Xue <rx.astro@gmail.com>
2016-09-20 09:55:45 -0400

    no message

---
+++ scripts/examples/config.py
@@ -0,0 +1,4 @@
+#
+#   SET SOME PROJECT-RELATED GLOBAL VARIABLES
+#
+sys.path.insert(1,os.path.dirname(xlib_path)+'/scripts/examples/')
---
+++ scripts/tests/config.py
@@ -0,0 +1,5 @@
+#
+#   SET SOME PROJECT-RELATED GLOBAL VARIABLES
+#
+
+sys.path.insert(1,os.path.dirname(xlib_path)+'/scripts/tests/')
